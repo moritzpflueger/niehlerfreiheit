0d087b3cebfe0a0aa3f2830804937c7f222c7233
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 00:31:23 +0100

    make event listitems a snippet and link to event page

--- site/snippets/components/upcomingEvents/list.php
+++ site/snippets/components/upcomingEvents/list.php
@@ -39,26 +39,7 @@
       </h3>
     <?php endif; ?>
     <?php foreach ($events as $event): ?>
-      <article class="mb-10 flex gap-5">
-        <?php if ($event->files()->valid()): ?>
-          <img 
-            src="<?= $event->files()->first()->url() ?>" 
-            alt="<?= $event->files()->first()->alt() ?>" 
-            class="h-32 w-32"
-          >
-        <?php else: ?>
-          <img 
-            src="<?= $site->placeholderEventImage()->toFile()->url() ?>" 
-            alt="Placeholder" 
-            class="h-32 w-32"
-          >
-        <?php endif; ?>
-        <div class="">
-          <p class="uppercase text-xl"><?= date('D j M', strtotime($event->date())) ?></p>
-          <h2 class="text-2xl font-bold"><?= $event->title()->html() ?></h2>
-          <p><?= $event->description()->excerpt(100) ?></p>
-        </div>
-      </article>
+      <?php snippet('components/upcomingEvents/listItem', ['event' => $event]) ?>
     <?php endforeach; ?>
   <?php endforeach; ?>
   <?php if ($showEventsLink): ?>
---
+++ site/snippets/components/upcomingEvents/listItem.php
@@ -0,0 +1,22 @@
+<article class="mb-10">
+  <a href="<?= $event->url() ?>" class="flex gap-5">
+    <?php if ($event->files()->valid()): ?>
+      <img 
+        src="<?= $event->files()->first()->url() ?>" 
+        alt="<?= $event->files()->first()->alt() ?>" 
+        class="h-32 w-32"
+      >
+    <?php else: ?>
+      <img 
+        src="<?= $site->placeholderEventImage()->toFile()->url() ?>" 
+        alt="Placeholder" 
+        class="h-32 w-32"
+      >
+    <?php endif; ?>
+    <div class="">
+      <p class="uppercase text-xl"><?= date('D j M', strtotime($event->date())) ?></p>
+      <h2 class="text-2xl font-bold"><?= $event->title()->html() ?></h2>
+      <p><?= $event->description()->excerpt(100) ?></p>
+    </div>    
+  </a>
+</article>
