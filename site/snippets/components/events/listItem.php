a06136edb33d5e298beb408502ff23c8e9d5eb77
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 17:26:43 +0100

    polish event list item

--- site/snippets/components/events/listItem.php
+++ site/snippets/components/events/listItem.php
@@ -12,13 +12,13 @@
 ?>
 
 <article class="mb-10">
-  <a href="<?= $eventUrl ?>" class="flex flex-col sm:flex-row gap-5">
+  <a href="<?= $eventUrl ?>" class="flex flex-col sm:flex-row gap-10">
     <img 
       src="<?= $imageUrl ?>" 
       alt="<?= $imageAlt ?>" 
-      class="h-32 w-32"
+      class="w-1/3"
     >
-    <div class="">
+    <div class="flex-1">
       <p class="uppercase text-xl"><?= $event->date()->toDate('EEE dd MMMM') ?></p>
       <h2 class="text-2xl font-bold"><?= $event->title()->html() ?></h2>
       <p><?= $event->description()->excerpt(100) ?></p>
