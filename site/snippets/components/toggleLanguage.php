0ecee8a284f676f1584c75864a682180b4558998
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 14:53:54 +0100

    polish language toggle

--- site/snippets/components/toggleLanguage.php
+++ site/snippets/components/toggleLanguage.php
@@ -1,7 +1,7 @@
 <ul class="flex gap-3 items-center">
   <?php foreach($kirby->languages() as $language): ?>
     <li <?php e($kirby->language() == $language, ' class="text-yellow-500"') ?> >
-      <a href="<?= $language->url() ?>" hreflang="<?= $language->code() ?>">
+      <a class="uppercase font-semibold" href="<?= $language->url() ?>" hreflang="<?= $language->code() ?>">
         <?= html($language->code()) ?>
       </a>
     </li>
