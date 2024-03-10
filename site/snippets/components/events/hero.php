23ef763c401742752b8332b926cff05560a913c5
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 15:26:34 +0100

    make hero event a link to event page

--- site/snippets/components/events/hero.php
+++ site/snippets/components/events/hero.php
@@ -3,7 +3,7 @@
   $today = date('Y-m-d');
 
   // Filter for future events, sort them by date, and retrieve the first event
-  $upcomingEvent = $eventsPage
+  $event = $eventsPage
     ->children()
     ->listed()
     ->filter(function($child) use ($today) {
@@ -12,29 +12,31 @@
     ->sortBy('date', 'asc')
     ->first();
 
-  if (!$upcomingEvent) return;
+  if (!$event) return;
 ?>
 
 <section class="my-32">
   <article class="flex flex-col gap-5">
-    <div class="">
-      <p class="font-bold uppercase text-4xl mb-5">
-        <?= $upcomingEvent->date()->toDate('E dd MMMM') ?>
-      </p>
-      <h2 class="text-5xl"><?= $upcomingEvent->title()->html() ?></h2>
-    </div>
-    <?php if ($upcomingEvent->files()->valid()): ?>
-      <img 
-        src="<?= $upcomingEvent->files()->first()->url() ?>" 
-        alt="<?= $upcomingEvent->files()->first()->alt() ?>" 
-        class="scale-110 my-10"
-      >
-    <?php else: ?>
-      <img 
-        src="<?= $site->placeholderEventImage()->toFile()->url() ?>" 
-        alt="Placeholder" 
-        class=""
-      >
-    <?php endif; ?>
+    <a href="<?= $event->url()?>">
+      <div class="">
+        <p class="font-bold uppercase text-4xl mb-5">
+          <?= $event->date()->toDate('E dd MMMM') ?>
+        </p>
+        <h2 class="text-5xl"><?= $event->title()->html() ?></h2>
+      </div>
+      <?php if ($event->files()->valid()): ?>
+        <img 
+          src="<?= $event->files()->first()->url() ?>" 
+          alt="<?= $event->files()->first()->alt() ?>" 
+          class="scale-110 my-10"
+        >
+      <?php else: ?>
+        <img 
+          src="<?= $site->placeholderEventImage()->toFile()->url() ?>" 
+          alt="Placeholder" 
+          class=""
+        >
+      <?php endif; ?>      
+    </a>
   </article>
 </section>
