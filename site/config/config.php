ab11f4bcd33a2b03edf0da0ae609a02af010d2a9
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 12:53:57 +0100

    enable i18n

--- site/config/config.php
+++ site/config/config.php
@@ -2,5 +2,8 @@
 
 return [
   'debug' => true,
+  'languages' => true,
+  'language.detect' => true,
+  'date.handler' => 'intl',
   'd4l.static_site_generator.endpoint' => 'generate-static-site'
 ];
