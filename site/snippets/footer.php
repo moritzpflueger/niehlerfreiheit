5c29d861569908a7590591eaf9e3bbbab73c60f1
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-10 14:55:04 +0100

    add social links and language toggle to footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,11 @@
-    <footer class="py-10 text-center">
-      <?= $site->footerText() ?>
+    <footer class="flex flex-col items-center py-10">
+      <div class="text-2xl flex gap-5">
+        <?php snippet('components/socialLinks') ?>
+        <?php snippet('components/toggleLanguage') ?>      
+      </div>
+      <div class="text-center py-10">
+        <?= $site->footerText() ?>
+      </div>
       <p>&copy; <?= date('Y') ?> Kalker Freiheit</p>
     </footer>
   </body>
