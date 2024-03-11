9df457cfcc016dc760ced7db5a23ae62c19086dc
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-11 16:43:36 +0100

    adjust default page

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,7 +2,7 @@
 <main class="flex-grow">
   <div class="">
     <h1 class="text-4xl my-10"><?= $page->title() ?></h1>
-    <?= $page->text() ?>      
+    <?= $page->text()->kirbyText() ?>      
   </div>
 </main>
 <?php snippet('footer') ?>
