6f6d0fc5493f0ab106e480ae21deaaee3333050d
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-11 15:49:31 +0100

    make sure all events are visible

--- site/templates/events.php
+++ site/templates/events.php
@@ -5,7 +5,7 @@
 <?= snippet('header') ?>
 <!-- <h1 class="text-5xl my-10"><?= $page->title() ?></h1> -->
 <?= snippet('components/events/list', [
-  'rows' => 10, 
+  'rows' => 1000, 
   'groupByMonth' => true,
   'showPastEvents' => $showPastEvents ?? false,
   'showPastEventsLink' => !$showPastEvents ?? true,
