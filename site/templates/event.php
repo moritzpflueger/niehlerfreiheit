ab954e8b630bdd2b9732c57880f6238540987b2f
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-12 10:10:45 +0100

    polish event page template

--- site/templates/event.php
+++ site/templates/event.php
@@ -1,17 +1,11 @@
 <?php snippet('header') ?>
-  <section class="my-32">
-    <article class="flex flex-col gap-5">
-      <div class="">
-        <p class="font-bold uppercase text-4xl mb-5">
-          <?= $page->date()->toDate('E dd MMMM') ?>
-        </p>
-        <h2 class="text-5xl"><?= $page->title()->html() ?></h2>
-      </div>
+  <section class="mb-32">
+    <article class="">
       <?php if ($page->files()->valid()): ?>
         <img 
           src="<?= $page->files()->first()->url() ?>" 
           alt="<?= $page->files()->first()->alt() ?>" 
-          class="my-10"
+          class="mb-10 w-full"
         >
       <?php else: ?>
         <img 
@@ -20,7 +14,15 @@
           class=""
         >
       <?php endif; ?>
+      <div class="my-10">
+        <p class="font-bold uppercase text-4xl mb-5">
+          <?= $page->date()->toDate('E dd MMMM') ?>
+        </p>
+        <h2 class="text-5xl"><?= $page->title()->html() ?></h2>
+      </div>
+      <div class="kirbytext">
         <?= $page->text()->kirbyText() ?>
+      </div>
     </article>
   </section>
 <?php snippet('footer') ?>
