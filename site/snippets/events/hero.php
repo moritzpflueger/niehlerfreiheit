84083ddd3e24f9ee1492fb21c4a8a927596eb6da
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-12 14:20:37 +0100

    polish hero event

--- site/snippets/events/hero.php
+++ site/snippets/events/hero.php
@@ -25,21 +25,27 @@
   };
 ?>
 
-<section class="my-32">
+<section class="my-16">
   <article class="flex flex-col gap-5">
     <a href="<?= $eventUrl?>">
       <div class="">
-        <p class="uppercase text-2xl mb-5">
+        <div class="uppercase text-2xl mb-5  inline-flex px-10">
           <?= $event->date()->toDate('E dd MMMM') ?>
-        </p>
-        <h2 class="text-5xl uppercase font-bold"><?= $event->title()->html() ?></h2>
+        </div>
+        <h2 class="text-6xl uppercase font-bold px-10 tracking-tight"><?= $event->title()->html() ?></h2>
       </div>
       <img 
         src="<?= $imageUrl ?>" 
         alt="<?= $imageAlt ?>" 
-        class="my-10 w-full"
+        class="mt-10 w-full"
       >
-      <p><?= $event->text()->excerpt(200) ?></p>
+      <!-- <p class="px-10"><?= $event->text()->excerpt(200) ?></p> -->
+      <a href="<?= $eventUrl ?>" class="text-yellow-500 text-xl hover:underline flex items-center gap-3 px-10 ml-auto">
+        VIEW DETAILS
+        <div class="w-10">
+          <?= file_get_contents(kirby()->root('assets') . '/icons/arrowRight.svg'); ?>
+        </div>
+      </a>
     </a>
   </article>
 </section>
