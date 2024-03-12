67e2f9c0027ca06189426de6fe0d99f330391368
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-12 10:45:19 +0100

    wrap in .kirbytext class

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,6 +2,9 @@
 <main class="flex-grow">
   <div class="">
     <h1 class="text-4xl uppercase my-10"><?= $page->title() ?></h1>
+    <div class="kirbytext">
+      <?= $page->text()->kirbyText() ?>
+    </div>     
   </div>
 </main>
 <?php snippet('footer') ?>
