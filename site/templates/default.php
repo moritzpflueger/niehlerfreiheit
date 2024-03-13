530663c1559ea2925de62c2a7abdf73276cb3df5
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-13 12:27:58 +0100

    decrease default page width

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,6 @@
 <?php snippet('header') ?>
 <main class="flex-grow">
-  <div class="">
+  <div class="mx-auto max-w-2xl">
     <h1 class="text-4xl uppercase my-10"><?= $page->title() ?></h1>
     <div class="kirbytext">
       <?= $page->text()->kirbyText() ?>
