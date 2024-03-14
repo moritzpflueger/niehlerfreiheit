b7c1c1bab4c85118d26160254004b240245e6c50
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-14 11:27:09 +0100

    use blocks for default pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,12 @@
 <?php snippet('header') ?>
   <main class="max-w-4xl mx-auto">
-    <h1><?= $page->title() ?></h1>
-    <div class="kirbytext">
-      <?= $page->text()->kirbyText() ?>
-    </div>     
+    <h1 class="text-5xl sm:text-6xl"><?= $page->title() ?></h1>
+    <div class="blocks">
+      <?php foreach ($page->blocks()->toBlocks() as $block): ?>
+        <div class="block" data-type="<?= $block->type() ?>">
+          <?= $block ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </main>
 <?php snippet('footer') ?>
