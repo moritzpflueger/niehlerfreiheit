eaf6fff7b06c0886b580aa368d156c372a6bbcae
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-14 11:15:11 +0100

    split events teaser into two snippets

---
+++ site/snippets/teasers/eventsList.php
@@ -0,0 +1,34 @@
+<?php
+  $rows = $rows ?? 3;
+  $eventsPage = $site->find('events');
+  $excludeHero = $excludeHero ?? true;
+
+  $events = $eventsPage
+    ->children()
+    ->listed()
+    ->filterBy('date', '>=', date('Y-m-d'))
+    ->sortBy('date', 'asc')
+    ->limit($rows);
+?>
+
+<h2>UPCOMING EVENTS</h2>
+
+<?php $index = 0; ?>
+<?php foreach ($events as $event): ?>
+  <?php snippet('events/listItem', [
+    'event' => $event,
+    'highlightFeaturedEvent' => true,
+    'showImage' => false,
+    'showDivider' => $index < count($events) - 1
+  ]) ?>
+  <?php $index++; ?>
+<?php endforeach; ?>
+
+<a href="<?= $site->url() ?>/events" class="text-yellow-500 text-xl uppercase hover:underline flex items-center justify-end gap-3">
+  <?= t('events.button.viewUpcoming') ?>
+  <div class="w-10">
+    <?= file_get_contents(kirby()->root('assets') . '/icons/arrowRight.svg'); ?>
+  </div>
+</a>
+
+<!-- text-[#00538A] -->
