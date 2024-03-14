2a63c32a4f9dca699532e21ed146f76159ebd4e3
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-14 09:40:06 +0100

    remove event hero section from list item

--- site/snippets/events/listItem.php
+++ site/snippets/events/listItem.php
@@ -13,48 +13,24 @@
   $showYear = $showYear ?? false;
 ?>
 
-<?php if ($highlightFeaturedEvent && $event->isFeatured()->isTrue()): ?>
-  <article class="flex flex-col gap-5 my-16">
-    <a href="<?= $eventUrl?>">
-      <div class="">
-        <div class="uppercase sm:text-2xl mb-5  inline-flex sm:px-10">
-          <?= $event->date()->toDate('E dd MMMM' . ($showYear ? ' Y' : '')) ?>
-        </div>
-        <h2 class="text-4xl sm:text-6xl uppercase font-bold sm:px-10 tracking-tight"><?= $event->title()->html() ?></h2>
-      </div>
-      <img 
-        src="<?= $imageUrl ?>" 
-        alt="<?= $imageAlt ?>" 
-        class="mt-10 w-full"
-      >
-      <!-- <p class="px-10"><?= $event->text()->excerpt(200) ?></p> -->
-      <a href="<?= $eventUrl ?>" class="text-yellow-500 text-xl hover:underline flex items-center gap-3 px-10 ml-auto">
-        VIEW DETAILS
-        <div class="w-10">
-          <?= file_get_contents(kirby()->root('assets') . '/icons/arrowRight.svg'); ?>
-        </div>
-      </a>
-    </a>
-  </article>
-<?php else: ?>
-  <article class="mb-20">
-    <a href="<?= $eventUrl ?>" class="flex flex-col sm:flex-row gap-10">
-      <div class="sm:w-1/4">
-        <img src="<?= $imageUrl ?>" alt="<?= $imageAlt ?>" />
-      </div>
-      <div class="flex-1">
-        <p class="sm:text-xl uppercase mb-3">
-          <?= $event->date()->toDate('E dd MMMM' . ($showYear ? ' Y' : '')) ?>
-        </p>
-        <h2 class="text-3xl sm:text-5xl uppercase font-bold mb-3"><?= $event->title()->html() ?></h2>
-        <p>Einlass: 19:00 Uhr</p>
-        <p>Abendkasse: 15 €</p>
-        <!-- <p><?= $event->text()->excerpt(100) ?></p> -->
-      </div>    
-    </a>
-  </article>
-<?php endif; ?>
+<article class="mb-20">
+  <a href="<?= $eventUrl ?>" class="flex flex-col sm:flex-row gap-10">
+    <div class="sm:w-1/4">
+      <img src="<?= $imageUrl ?>" alt="<?= $imageAlt ?>" />
+    </div>
+    <div class="flex-1">
+      <p class="sm:text-2xl uppercase mb-2 bg-white text-black inline-flex px-1">
+        <?= $event->date()->toDate('E dd MMMM' . ($showYear ? ' Y' : '')) ?>
+      </p>
+      <h2 class="text-3xl sm:text-4xl uppercase italic font-bold mb-3"><?= $event->title()->html() ?></h2>
+      <p>Einlass: 19:00 Uhr</p>
+      <p>Abendkasse: 15 €</p>
+      <!-- <p><?= $event->text()->excerpt(100) ?></p> -->
+    </div>    
+  </a>
+</article>
 
+<!-- bg-[#00538A] -->
 
 
 
--- site/snippets/teasers/events.php
+++ site/snippets/teasers/events.php
@@ -23,16 +23,43 @@
 
 <section class="my-32">
   <?php foreach ($events as $event): ?>
-    <?php snippet('events/listItem', [
-      'event' => $event,
-      'highlightFeaturedEvent' => true,
-    ]) ?>
+    <?php if ($event === $events->first()): ?>
+      <article class="flex flex-col gap-5 my-16">
+        <a href="<?= $event->url()?>" class="relative">
+          <div class="px-10">
+            <div class="uppercase sm:text-3xl mb-2 bg-white text-black filter inline-flex">
+              <?= $event->date()->toDate('EEEE dd MMMM') ?>
+            </div>
+            <h2 class="text-4xl sm:text-6xl uppercase font-bold italic tracking-tight"><?= $event->title()->html() ?></h2>
+          </div>
+          <img 
+            src="<?= $event->files()->first()->url(); ?>" 
+            alt="<?= $event->files()->first()->alt(); ?>" 
+            class="mt-10"
+          >
+          <!-- <p class="px-10"><?= $event->text()->excerpt(200) ?></p> -->
+          <a href="<?= $event->url() ?>" class="text-yellow-500 text-xl hover:underline flex items-center gap-3 px-10 ml-auto">
+            VIEW DETAILS
+            <div class="w-10">
+              <?= file_get_contents(kirby()->root('assets') . '/icons/arrowRight.svg'); ?>
+            </div>
+          </a>
+        </a>
+      </article>
+    <?php else: ?>
+      <?php snippet('events/listItem', [
+        'event' => $event,
+        'highlightFeaturedEvent' => true,
+        ]) ?>
+    <?php endif; ?>
   <?php endforeach; ?>
 
-  <a href="<?= $site->url() ?>/events" class="text-yellow-500 text-xl uppercase hover:underline flex items-center gap-3">
+  <a href="<?= $site->url() ?>/events" class="text-yellow-500 text-xl uppercase hover:underline flex items-center justify-end gap-3">
     <?= t('events.button.viewUpcoming') ?>
     <div class="w-10">
       <?= file_get_contents(kirby()->root('assets') . '/icons/arrowRight.svg'); ?>
     </div>
   </a>
 </section>
+
+<!-- text-[#00538A] -->
