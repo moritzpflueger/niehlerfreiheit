ddcd3a1a2f1075c2eabc8d27adbe05ba5f9bc3bd
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-04-05 15:32:25 +0200

    show time fields in event list item

--- site/snippets/events/listItem.php
+++ site/snippets/events/listItem.php
@@ -23,16 +23,16 @@
       </div>
     <?php endif; ?>
     <div class="flex-1">
-      <!-- <p class="sm:text-2xl uppercase mb-2 bg-white text-black inline-flex px-1">
+      <div class="bg-pink-400 text-black text-base inline-flex px-1 -skew-x-12 ml-1 mb-3"><?= $event->category() ?></div>
+      <div class="sm:text-2xl uppercase mb-2">
         <?= $event->date()->toDate('E dd MMMM' . ($showYear ? ' Y' : '')) ?>
-      </p> -->
-      <p class="sm:text-2xl uppercase mb-2 inline-flex px-1">
-        <?= $event->date()->toDate('E dd MMMM' . ($showYear ? ' Y' : '')) ?>
-      </p>
+      </div>
       <h3><?= $event->title()->html() ?></h3>
       <!-- <h2 class="text-3xl sm:text-4xl uppercase italic font-bold mb-3"><?= $event->title()->html() ?></h2> -->
-      <p>Einlass: 19:00 Uhr</p>
-      <p>Abendkasse: 15 €</p>
+      <?php if (!$event->admissiontime()->isEmpty()): ?>
+        <p><?= t('event.admissionTime') . ': ' . $event->admissionTime()->toDate('HH:mm') ?></p>
+      <?php endif ?>
+      <p><?= t('event.startTime') . ': ' . $event->starttime()->toDate('HH:mm') ?></p>
       <!-- <p><?= $event->text()->excerpt(100) ?></p> -->
     </div>    
   </a>
