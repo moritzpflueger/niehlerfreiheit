a34b14c4bff9ced406c6bc06f944461a602a3cb0
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-04-13 12:52:56 +0200

    add helpful comment

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,7 +5,11 @@
 <main class="max-w-4xl mx-auto">
   <?php foreach ($homepage->blocks()->toBlocks() as $block): ?>
     <section class="block my-20" data-type="<?= $block->type() ?>">
-      <?= $block ?>
+      <?= $block ?> 
+      <!-- This is some kirby magic, 
+      that when you have a snippet
+      in the snippets/blocks folder 
+      named like the block type. -->
     </section>
   <?php endforeach; ?>
 </main>
