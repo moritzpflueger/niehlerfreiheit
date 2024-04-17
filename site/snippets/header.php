c53e627d435ae57141530e6372891fb569e8facc
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-04-17 16:05:22 +0200

    test deploy

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -17,7 +17,7 @@
   </head>
   <body>
     <header class="max-w-4xl w-full mx-auto py-5 md:py-10">
-      test7
+      test8
       <nav class="flex items-start justify-between">
         <a href="<?= $site->url() ?>">
           <img 
