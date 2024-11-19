3a2f16ceb55135f0653e6bc2c8668221f3a65e42
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-11-19 14:15:06 +0100

    Fix border not aligned perfectly

--- site/snippets/blocks/hero.php
+++ site/snippets/blocks/hero.php
@@ -1,10 +1,10 @@
 <div class="grid sm:grid-cols-2 border-b border-neutral-700">
-  <div class="col-span-1 flex items-center justify-center px-2 py-2 aspect-square">
+  <div class="col-span-1 flex items-center justify-center px-2 py-2 aspect-square sm:border-r border-neutral-700">
     <p class="px-3 pt-2 max-w-xl">
       <?= $block->text() ?>
     </p>
   </div>
-  <div class="col-span-1 border-t sm:border-t-0 sm:border-l border-neutral-700 aspect-square">
+  <div class="col-span-1 border-t sm:border-t-0 border-neutral-700 aspect-square">
     <img
       src="<?= $block->image()->toFile()->url() ?>"
       alt="<?= $block->image()->toFile()->alt() ?>"
