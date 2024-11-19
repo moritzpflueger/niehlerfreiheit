e50a2496e3f6cb715ee8f7a72d5770549f76bea8
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-11-19 15:14:51 +0100

    Fix icon paths

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,12 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="<?= $kirby->url() . '/assets/favicon.ico' ?>" type="image/x-icon">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
-  <link rel="manifest" href="/manifest.json">
-  <link rel="mask-icon" href="assets/safari-pinned-tab.svg" color="#000000">
+  <link rel="shortcut icon" href="<?= $kirby->url() . '/assets/icons/favicon.ico' ?>" type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/icons/favicon-16x16.png">
+  <link rel="mask-icon" href="assets/icons/safari-pinned-tab.svg" color="#000000">
   <meta name="msapplication-TileColor" content="#000000">
   <meta name="theme-color" content="#000000">
   <title>Niehler Freiheit e.V.</title>
