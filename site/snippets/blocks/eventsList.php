6ad39e00126fa31de7b247e0e017ba6d60a5ca55
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-12-10 18:55:46 +0100

    Fix events teaser looks shitty with not few events

--- site/snippets/blocks/eventsList.php
+++ site/snippets/blocks/eventsList.php
@@ -10,11 +10,27 @@ $events = $eventsPage
   ->filterBy('date', '>=', date('Y-m-d'))
   ->sortBy('date', 'asc')
   ->limit($rows);
+
+$gridClass = '';
+switch (count($events)) {
+  case 3:
+    $gridClass = 'sm:grid-cols-4';
+    break;
+  case 2:
+    $gridClass = 'sm:grid-cols-3';
+    break;
+  case 1:
+    $gridClass = 'sm:grid-cols-2';
+    break;
+  default:
+    $gridClass = 'sm:grid-cols-1';
+    break;
+}
 ?>
 
 <!-- <h2 class="text-xl mb-0 uppercase py-32 flex items-center text-center justify-center border-b border-neutral-700"><?= $heading ?></h2> -->
 
-<div class="grid sm:grid-cols-2 lg:grid-cols-4">
+<div class="grid <?= $gridClass ?>">
   <?php $index = 0; ?>
   <?php foreach ($events as $event): ?>
     <?php snippet('events/listItem', [
