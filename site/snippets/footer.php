78dfc5bb493427bc3468bd05d1da3f6a463a1a3a
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-12-12 13:59:58 +0100

    Update footer layout

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,7 +1,7 @@
-    <footer class="grid sm:grid-cols-2 gap-y-8 border-t border-neutral-700">
+    <footer class="grid sm:grid-cols-2 border-t border-neutral-700">
 
 
-      <div class="flex flex-col gap-y-8 col-span-1 sm:border-r border-neutral-700 px-3 py-2">
+      <div class="flex flex-col gap-y-8 col-span-1 sm:border-r border-neutral-700 px-3 py-2 justify-between">
         <ul>
           <li>
             <ul>
@@ -23,6 +23,10 @@
             </ul>
           </li>
         </ul>
+      </div>
+
+      <div class="flex flex-col-reverse sm:flex-col gap-y-4 sm:gap-y-8 col-span-1 px-3 py-2 justify-between">
+        <?php snippet('components/socialLinks') ?>
         <ul class="flex flex-col w-full">
           <li>
             <a href="<?= $site->url() ?>/events?showPastEvents=true"><?= $kirby->language()->code() === 'en' ? 'Archive' : 'Archiv' ?></a>
@@ -35,12 +39,16 @@
           </li>
         </ul>
       </div>
-      <div class="col-span-1 px-3 py-2 flex flex-col justify-between">
-        <?php snippet('components/socialLinks') ?>
-        <div>
-          <?php snippet('components/toggleLanguage') ?>
-          &copy;<?= date('Y') ?> Niehler Freiheit
-        </div>
+
+      <div class="col-span-1 px-3 py-2 sm:border-r border-neutral-700">
+        <?php snippet('components/toggleLanguage') ?>
+        <div>&copy;<?= date('Y') ?> Niehler Freiheit</div>
+      </div>
+
+      <div class="col-span-1 px-3 py-2 flex items-end">
+        <span>
+          Made with ♥ by <a class="inline-block underline" href="https://www.moritzpflueger.com/en/">Moritz Pflüger</a>
+        </span>
       </div>
     </footer>
     </body>
